95143fc76555843bed1aa8f8d72ebf83444c6b8f
Suraj Rao <suraj_rao@berkeley.edu>
2024-12-11 17:31:42 -0800

    current status of modeling code

--- notebooks/models.py
+++ notebooks/models.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn import svm
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, GridSearchCV
 from sklearn.linear_model import LogisticRegression, LinearRegression, Lasso, Ridge
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import log_loss, confusion_matrix, mean_squared_error, accuracy_score
+from sklearn.metrics import classification_report, log_loss, confusion_matrix, mean_squared_error, accuracy_score
 from scipy.stats import randint
+from statsmodels.miscmodels.ordinal_model import OrderedModel
 import re
+import xgboost as xgb
 
 class BaseModel:
     def __init__(self, data, regularization=None):
@@ -19,8 +22,9 @@ class BaseModel:
         self.regularization = regularization
     
     def preprocess(self, features):
-        features.append("target")
-        self.data = self.data[features]
+        all_features = features + ['target']
+        self.data = self.data[all_features]
+        self.data = self.data.dropna()
 
         # Split data into training and testing sets
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
@@ -63,35 +67,91 @@ class LogisticRegressionModel(BaseModel):
         print("The log loss is: ", log_loss(self.y_test, y_pred))
         print("The confusion matrix is: ", confusion_matrix(self.y_test, y_pred))
 
-# TODO: Need to create a multi-classification model for handling ties
+class XGBoostModel(BaseModel):
+    def __init__(self, data):
+        super().__init__(data, regularization=None)
+    
+    def preprocess(self, features):
+        self.data['target'] = self.data['winner'].map({'model_a': 0, 'model_b': 1, 'tie': 2, 'tie (bothbad)': 3})
+        super().preprocess(features)
 
+    def train_model(self):
+        self.model = xgb.XGBClassifier()
+        self.model.fit(self.X_train, self.y_train)
+    
+    def predict(self, test_data=None):
+        if test_data is not None:
+            y_pred = self.model.predict(test_data)
+            return y_pred
+        y_pred = self.model.predict(self.X_test)
+        return y_pred
+    
+    def evaluate(self, y_pred):
+        acc = accuracy_score(self.y_test, y_pred)
+        print("The accuracy is: ", acc)
+        print("Classification report: ", classification_report(self.y_test, y_pred))
+        return acc
+
+class SVMModel(BaseModel):
+    def __init__(self, data, decision_function_shape='ovr'):
+        super().__init__(data, regularization=None)
+        self.decision_function_shape = decision_function_shape
+        self.C = None
+    
+    def preprocess(self, features):
+        # Add column as target for classification
+        self.data['target'] = self.data['winner']
+        super().preprocess(features)
+
+    def train_model(self):
+        self.model = svm.SVC(decision_function_shape=self.decision_function_shape)
+        self.tune_hyperparameters()
+
+    def predict(self, test_data=None):
+        if test_data is not None:
+            y_pred = self.model.predict(test_data)
+            return y_pred
+        y_pred = self.model.predict(self.X_test)
+        return y_pred
+    
+    def tune_hyperparameters(self):
+        params = {'C': [0.1, 10, 100],  
+              'gamma': [1, 0.1, 0.01], 
+              'kernel': ['rbf']} 
+        grid_search = GridSearchCV(self.model, params, cv=5)
+        grid_search.fit(self.X_train, self.y_train)
+        print("Best parameters: ", grid_search.best_params_)
+        self.model = grid_search.best_estimator_
+        self.C = grid_search.best_params_['C']
+
+    def evaluate(self, y_pred):
+        acc = accuracy_score(self.y_test, y_pred)
+        print("The accuracy is: ", acc)
+        print("Classification report: ", classification_report(self.y_test, y_pred))
+        return acc
 
 
 class MultiLinearRegressionModel(BaseModel):
     def __init__(self, data, regularization=None):
         super().__init__(data, regularization)
+        self.alpha = None
     
     def preprocess(self, features):
-        # Add column as target for classification
-        self.create_avg_hardness()
+        self.data = self.data.dropna() #TODO: take this out later
+        self.data['target'] = self.data['combined_hardness_score'].round().astype(int)
         super().preprocess(features)
 
-    def create_avg_hardness(self):
-        def extract_digit(value):
-            match = re.search(r"\d", str(value))
-            return int(match.group()) if match else None
-        for col in ["score_value_1", "score_value_2", "score_value_3"]:
-            self.data[col] = self.data[col].apply(extract_digit)
-        self.data['target'] = round(self.data[["score_value_1", "score_value_2", "score_value_3"]].mean(axis=1, skipna=True))
-
-    def train_model(self):
+    def train_model(self, tune_hyperparameters=False):
         if self.regularization == 'l1':
             self.model = Lasso()
         elif self.regularization == 'l2':
             self.model = Ridge()
         else:
             self.model = LinearRegression()
-        self.model.fit(self.X_train, self.y_train)
+        if tune_hyperparameters:
+            self.tune_hyperparameters()
+        else:
+            self.model.fit(self.X_train, self.y_train)
 
     def predict(self, test_data=None):
         if test_data is not None:
@@ -99,13 +159,53 @@ class MultiLinearRegressionModel(BaseModel):
             return y_pred
         y_pred = np.array(self.model.predict(self.X_test)).round()
         return y_pred
+    
+    def tune_hyperparameters(self):
+        if self.regularization == None:
+            self.model.fit(self.X_train, self.y_train)
+            return
+        params = {'alpha': np.arange(0.02, 1, 0.02)}
+        grid_search = GridSearchCV(self.model, params, cv=5)
+        grid_search.fit(self.X_train, self.y_train)
+        print("Best parameters: ", grid_search.best_params_)
+        # TODO: Do i need to set the model to the best estimator?
+        self.model = grid_search.best_estimator_
+        self.alpha = grid_search.best_params_['alpha']
 
     def evaluate(self, y_pred):
-        print("The MSE is: ", mean_squared_error(self.y_test, y_pred))
+        mse = mean_squared_error(self.y_test, y_pred)
+        print("The MSE is: ", mse)
+        return mse
+    
+# Ordinal regression model is in the works currently    
+class OrdinalRegressionModel(BaseModel):
+    def __init__(self, data):
+        super().__init__(data, regularization=None)
 
+    def preprocess(self, features):
+        self.data = self.data.dropna() #TODO: take this out later
+        self.data['target'] = self.data['combined_hardness_score'].round().astype(int)
+        return super().preprocess(features)
+
+    def train_model(self):
+        model = OrderedModel(self.y_train, self.X_train, distr='probit')
+        self.model = model.fit(method='bfgs')
+    
+    def predict(self, test_data=None):
+        if test_data is not None:
+            y_pred = self.model.model.predict(self.model.params, test_data)
+        else:
+            y_pred = self.model.model.predict(self.model.params, self.X_test)
+        return y_pred.argmax(1)
+
+    def evaluate(self, y_pred):
+        mse = mean_squared_error(self.y_test, y_pred)
+        print("The MSE is: ", mse)
+        return mse
+    
 class RandomForestModel(BaseModel):
     def __init__(self, data):
-        super().__init__(data, None)
+        super().__init__(data, regularization=None)
     
     def preprocess(self, features):
         # Add column as target for classification
@@ -130,79 +230,51 @@ class RandomForestModel(BaseModel):
 
 ### Below this line is not finished yet ###
 
-# Create scaffolding for hyperparameter tuning
-def tune_hyperparameters(model, data):
-    for regularization in [None, 'l1', 'l2']:
-        model.regularization = regularization
-        model.preprocess()
-        model.train_model()
-        y_pred = model.predict()
-        model.evaluate(y_pred)
 
-def rf_hyperparameter_tuning(data):
-    rf = RandomForestClassifier()
-    rand_search = RandomizedSearchCV(
-        rf,
-        {
-            'n_estimators': randint(1, 200),
-            'max_depth': randint(1, 200),
-            'min_samples_split': randint(2, 200),
-            'min_samples_leaf': randint(1, 200),
-            'max_features': ['auto', 'sqrt', 'log2'],
-            'bootstrap': [True, False]
-        },
-        n_iter=100,
-        cv=3,
-    )
-    rand_search.fit(X_train, y_train)
+# def rf_hyperparameter_tuning(data):
+#     rf = RandomForestClassifier()
+#     rand_search = RandomizedSearchCV(
+#         rf,
+#         {
+#             'n_estimators': randint(1, 200),
+#             'max_depth': randint(1, 200),
+#             'min_samples_split': randint(2, 200),
+#             'min_samples_leaf': randint(1, 200),
+#             'max_features': ['auto', 'sqrt', 'log2'],
+#             'bootstrap': [True, False]
+#         },
+#         n_iter=100,
+#         cv=3,
+#     )
+#     rand_search.fit(X_train, y_train)
 
-    print(rand_search.best_params_)
-    print(rand_search.best_score_)
-    print(rand_search.best_estimator_)
-    y_pred = rand_search.best_estimator_.predict(X_test)
+#     print(rand_search.best_params_)
+#     print(rand_search.best_score_)
+#     print(rand_search.best_estimator_)
+#     y_pred = rand_search.best_estimator_.predict(X_test)
 
-def lr_hyperparameter_tuning(data):
-    lr = LogisticRegression()
-    rand_search = RandomizedSearchCV(
-        lr,
-        {
-            'penalty': ['l1', 'l2', 'none'],
-            'C': np.logspace(-4, 4, 20),
-            'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
-        },
-        n_iter=100,
-        cv=3,
-    )
-    rand_search.fit(X_train, y_train)
+# def lr_hyperparameter_tuning(data):
+#     lr = LogisticRegression()
+#     rand_search = RandomizedSearchCV(
+#         lr,
+#         {
+#             'penalty': ['l1', 'l2', 'none'],
+#             'C': np.logspace(-4, 4, 20),
+#             'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
+#         },
+#         n_iter=100,
+#         cv=3,
+#     )
+#     rand_search.fit(X_train, y_train)
 
-    print(rand_search.best_params_)
-    print(rand_search.best_score_)
-    print(rand_search.best_estimator_)
-    y_pred = rand_search.best_estimator_.predict(X_test)
+#     print(rand_search.best_params_)
+#     print(rand_search.best_score_)
+#     print(rand_search.best_estimator_)
+#     y_pred = rand_search.best_estimator_.predict(X_test)
 
-def mlr_hyperparameter_tuning(data):
-    mlr = LinearRegression()
-    params = {'alpha': [0.1, 1.0, 10.0, 100.0]}
-    grid_search = GridSearchCV(mlr, params, cv=5)
-    rand_search = RandomizedSearchCV(
-        mlr,
-        {
-            'alpha': np.logspace(-4, 4, 20),
-            'l1_ratio': np.linspace(0, 1, 20)
-        },
-        n_iter=100,
-        cv=3,
-    )
-    rand_search.fit(X_train, y_train)
 
-    print(rand_search.best_params_)
-    print(rand_search.best_score_)
-    print(rand_search.best_estimator_)
-    y_pred = rand_search.best_estimator_.predict(X_test)
 
-# run cross fold validation
-# def cross_fold_validation(model, data):
-    
+# run cross fold validation    
     
 
 # Need to account for actual parameters in the regularization models
